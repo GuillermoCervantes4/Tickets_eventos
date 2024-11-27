2872012ac5e8633c75dac3a36953e8fa8837ce63
Diego Angulo <diego.angulo.c@ucb.edu.bo>
2024-11-27 19:11:10 -0400

    Ficha parcialmente hecha

--- src/app/pages/ficha/ficha.component.ts
+++ src/app/pages/ficha/ficha.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Evento } from '../../core/modelo/evento';
+import { CommonModule } from '@angular/common';
+import { DatabaseService } from '../../services/database.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-ficha',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './ficha.component.html',
   styleUrl: './ficha.component.scss'
 })
-export class FichaComponent {
+export class FichaComponent implements OnInit {
+  eventos: Evento[] = [];
 
+  constructor(private db: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  getEventos(): void {
+    this.db.fetchLocalCollection('datos').subscribe({
+      next: (data: Evento[]) => {
+        this.eventos = data;
+        console.log('Eventos cargados:', this.eventos);
+        this.db.fetchLocalCollection('datos')
+        .subscribe((res: any)=>{
+          res.forEach((items: any) => {
+            this.db.addFirestoreDocument('eventos',items)
+          });
+        })
+      }, 
+      error: (e: any) => {
+        console.error('Error al cargar eventos:', e);
+      }
+    });
+  }
+  
+  trackById(index: number, item: Evento): any {
+    return item.id;
+  }
 }
+
